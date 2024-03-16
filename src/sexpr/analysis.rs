8f3b2496607cd82e5bc7b2c51190c924c88e0849
Arnold deVos <adv+gh@backgroundsignal.com>
2024-03-16 16:20:39 +1100

    update analysis to use new combinators

--- src/sexpr/analysis.rs
+++ src/sexpr/analysis.rs
@@ -4,7 +4,7 @@ pub fn trim(x: &Expr) -> Option<Expr> {
     Some(x.as_atom()?.as_string()?.trim().into())
 }
 
-pub fn property(name: &str) -> impl Simplifier + Clone {
+pub fn property(name: &str) -> impl Simplifier {
     let name = name.to_owned();
 
     let key = move |x: &Expr| {
@@ -45,7 +45,19 @@ pub fn footprints() -> impl Simplifier {
 }
 
 pub fn symbols() -> impl Simplifier {
-    let properties = || {
+    let ignore_power =
+        |x: &Expr| (!x.as_atom()?.as_string()?.starts_with("power:")).then_some(x.clone());
+
+    let lib_id = || Cons("lib_id", Cons(ignore_power, Anything));
+
+    let attribs = {
+        Cons("in_bom", Anything)
+            .or(Cons("unit", Anything))
+            .or(Cons("dnp", Anything))
+            .or(lib_id())
+    };
+
+    let properties = {
         property("footprint")
             .or(property("reference"))
             .or(property("value"))
@@ -55,32 +67,20 @@ pub fn symbols() -> impl Simplifier {
             .or(property("description"))
     };
 
-    let attribs = || {
-        Cons("in_bom", Anything)
-            .or(Cons("unit", Anything))
-            .or(Cons("dnp", Anything))
-    };
-
-    let ignore_power =
-        |x: &Expr| (!x.as_atom()?.as_string()?.starts_with("power:")).then_some(x.clone());
-
-    let lib_id = || Cons("lib_id", Cons(ignore_power, Anything));
-
-    let body = || Cons(lib_id(), Filter(properties().or(attribs())));
-
     let symbol = Cons(
         Discard("symbol"),
-        body().or(Cons(Discard(Anything), body())),
+        Filter(attribs.or(properties)).and(Ensure(Find(lib_id()))),
     );
 
     Cons(Discard("kicad_sch"), Filter(symbol))
 }
 
 pub fn sheets() -> impl Simplifier {
-    let properties = Or(
-        property("sheetname"),
-        Or(property("sheetfile"), property("sheet file")),
-    );
+    let properties = property("sheetname")
+        .or(property("sheetfile"))
+        .or(property("sheet file"));
+
     let sheet = Cons(Discard("sheet"), Filter(properties));
+
     Cons(Discard("kicad_sch"), Filter(sheet))
 }
